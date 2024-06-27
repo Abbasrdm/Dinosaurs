79a77cfb1b3c6060eefb173e6929897e61630a0f
Abbas Ardam <Abbas.ardam@gmail.com>
2024-06-27 18:42:43 +0200

    First test

--- Dinosaurs.cpp
+++ Dinosaurs.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
-int main() {
+void testTyrannosaurus() {
+    Dinosaur* dino = new Tyrannosaurus();
+    string expectedAttack = "stomps with its legs.";
+    string expectedRoar = "ROAR!";
+    assert(dino->attack() == expectedAttack);
+    assert(dino->roar() == expectedRoar);
+    delete dino;
+}
 
+int main() {
+    testTyrannosaurus();
     return 0;
 }
